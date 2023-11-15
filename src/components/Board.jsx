da6cabcd2c55d4aa051bb4f142ca9fccdce38d87
mHaines331 <mhaines9219@gmail.com>
2023-11-15 12:28:25 -0800

    sync code

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,23 +1,32 @@
-import React from "react";
+import React, { useState } from 'react';
+import Square from './Square';
 
-export default function Board() {
-  return (
-    <>
-      <div className="board-row">
-        <button className="square">1</button>
-        <button className="square">2</button>
-        <button className="square">3</button>
-      </div>
-      <div className="board-row">
-        <button className="square">4</button>
-        <button className="square">5</button>
-        <button className="square">6</button>
-      </div>
-      <div className="board-row">
-        <button className="square">7</button>
-        <button className="square">8</button>
-        <button className="square">9</button>
-      </div>
-    </>
-  );
+export default function Board({playerTurn, setPlayerTurn}) {
+    const [squareVal, setSquareVal] = useState();
+    function checkGameOver() {
+        console.log('Run check win function')
+    }
+    function handleClick() {
+        alert('clicked!')
+        checkGameOver();
+    }
+    return (
+        <>
+            <div className="board-row">
+                <button className="square" onClick={handleClick}>1</button>
+                <button className="square" onClick={handleClick}>2</button>
+                <button className="square" onClick={handleClick}>3</button>
+            </div>
+            <div className="board-row">
+                <button className="square" onClick={handleClick}>4</button>
+                <button className="square" onClick={handleClick}>5</button>
+                <button className="square" onClick={handleClick}>6</button>
+            </div>
+            <div className="board-row">
+                <button className="square" onClick={handleClick}>7</button>
+                <button className="square" onClick={handleClick}>8</button>
+                <button className="square" onClick={handleClick}>9</button>
+            </div>
+        </>
+    )
 }
